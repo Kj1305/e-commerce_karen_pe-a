a50a7f037b1a4e59650752ab9fd23229421189a6
karen <karen@gmail.com>
2024-09-06 15:37:35 -0500

    fourth commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const h4 = document.querySelector("h4")
+const text = document.querySelector(".li-productos")
+const img = document.querySelector(".ofertas-especiales img")
+h4.innerText = "producto en promocion";
+text.innerText = "descripcion del producto";
+img.scr="https://th.bing.com/th/id/OIP.Yim6XG9z3w7vRGTt0AipJwHaE8?w=247&h=180&c=7&r=0&o=5&pid=1.7";
+
