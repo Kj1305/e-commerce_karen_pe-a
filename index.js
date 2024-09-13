9c9012496b162aceb9e107c7c7b5993616aee82a
karen <karen@gmail.com>
2024-09-13 14:31:49 -0500

    commit 6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const h4 = document.querySelector("h4")
 const text = document.querySelector(".li-productos")
-const img = document.querySelector(".ofertas-especiales img")
+const img = document.querySelector(".contenedor-ofertas img")
 h4.innerText = "producto en promocion";
 text.innerText = "descripcion del producto";
-img.scr="https://th.bing.com/th/id/OIP.Yim6XG9z3w7vRGTt0AipJwHaE8?w=247&h=180&c=7&r=0&o=5&pid=1.7";
+img.scr="https://th.bing.com/th/id/R.be219d27c92d83120dfa97647baeafc8?rik=CIaoMpCWEUV%2bzg&pid=ImgRaw&r=0";
 
