03c953692c9ab1069078f8d1ea847ffe196b0377
karen <karen@gmail.com>
2024-09-15 15:16:53 -0500

    commit 8

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// Seleccionar el h1 y agregarle el texto 'Productos'
+function cardsdeproductos(containerSelector, numerodeCards) {
 const h1 = document.querySelector("h1");
 h1.innerText = "Productos";
 
-// Seleccionar el contenedor de la sección con clase 'container'
+
 const container = document.querySelector(".container");
 
-// Crear un array vacío para almacenar las cards
 const cardsArray = [];
 
-// Generar las cards dinámicamente
-for (let i = 1; i <= 6; i++) {
+for (let i = 1; i <= numerodeCards; i++) {
     let card = `
         <div class="card" style="max-width: 2000px; margin: 20px; text-align: center; padding: 20px; background-color:white; border-radius: 15px; box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);">
             <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" 
@@ -21,12 +19,11 @@ for (let i = 1; i <= 6; i++) {
         </div>
     `;
 
-    // Agregar la card al array
     cardsArray.push(card);
 }
 
-// Asignar todas las cards a la sección
 container.innerHTML = cardsArray.join('');
+}
 
 
-
+cardsdeproductos(".container", 8)
