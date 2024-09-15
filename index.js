5c233b1dbd7ebbec1184290d79467b2ff2f868c0
karen <karen@gmail.com>
2024-09-15 12:13:20 -0500

    seventh commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cardsArray = [];
 // Generar las cards dinámicamente
 for (let i = 1; i <= 6; i++) {
     let card = `
-        <div class="card" style="max-width: 900px; margin: 20px; text-align: center; padding: 20px; background-color:white; border-radius: 15px; box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);">
+        <div class="card" style="max-width: 2000px; margin: 20px; text-align: center; padding: 20px; background-color:white; border-radius: 15px; box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);">
             <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" 
                  alt="Producto ${i}" 
                  style="width: 100%; height: auto; border-radius: 10px; object-fit: cover;">
